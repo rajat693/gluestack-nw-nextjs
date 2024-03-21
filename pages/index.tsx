920260e73b31b24095b2c330722313997fcc4ff3
Rajat Chaudhary <rajatchaudhary@Rajats-MacBook-Air.local>
2024-03-21 11:59:55 +0530

    feat: added nativewind setup

---
+++ pages/index.tsx
@@ -0,0 +1,7 @@
+export default function Home() {
+  return (
+    <main className="h-screen justify-center items-center flex">
+      <p className="text-green-500 text-xl font-bold">hello world</p>
+    </main>
+  );
+}
